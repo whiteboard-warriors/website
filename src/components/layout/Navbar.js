a2ba17c5c23a542b4353cf29611651f24d9169f5
Pablo <pmotta.dev@gmail.com>
2020-02-25 18:21:12 -0800

    installed react-bootstrap

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav>
+                <div className='nav-wrapper'>
+                    <a href='#!' className='brand-logo'>
+                        Logo
+                    </a>
+                    <a
+                        href='#'
+                        data-target='mobile-demo'
+                        className='sidenav-trigger'
+                    >
+                        <i className='material-icons'>menu</i>
+                    </a>
+                    <ul className='right hide-on-med-and-down'>
+                        <li>
+                            <a href='sass.html'>Sass</a>
+                        </li>
+                        <li>
+                            <a href='badges.html'>Components</a>
+                        </li>
+                        <li>
+                            <a href='collapsible.html'>Javascript</a>
+                        </li>
+                        <li>
+                            <a href='mobile.html'>Mobile</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+            <ul className='sidenav' id='mobile-demo'>
+                <li>
+                    <a href='sass.html'>Sass</a>
+                </li>
+                <li>
+                    <a href='badges.html'>Components</a>
+                </li>
+                <li>
+                    <a href='collapsible.html'>Javascript</a>
+                </li>
+                <li>
+                    <a href='mobile.html'>Mobile</a>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default Navbar;
