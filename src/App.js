2d1f2b86d47aa22e7c7779ed4869dca9b739968e
Pablo <pmotta.dev@gmail.com>
2020-02-29 17:26:24 -0800

    crated footer boiler plate

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React, { Fragment } from 'react';
 // router
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // bootstrap
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Image from 'react-bootstrap/Image';
+// import Container from 'react-bootstrap/Container';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+// import Image from 'react-bootstrap/Image';
 // pages
 import Landing from './components/pages/Landing';
 // components
@@ -15,7 +15,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.scss';
 // images
 // import logo from './img/logo/ww_logo./svg';
-import wireframe from './img/wireframes/wireframe_1.png';
+// import wireframe from './img/wireframes/wireframe_1.png';
 
 function App() {
     return (
@@ -23,14 +23,7 @@ function App() {
             <NavBar></NavBar>
             <Router>
                 <Switch>
-                    <Route exact path='/landing' component={Landing} />
-                    <Container>
-                        <Row>
-                            <Col md={{ span: 4, offset: 4 }}>
-                                <Image src={wireframe} fluid />
-                            </Col>
-                        </Row>
-                    </Container>
+                    <Route exact path='/' component={Landing} />
                 </Switch>
             </Router>
         </Fragment>
--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../header/Header';
+import Footer from '../layout/Footer';
 // bootstrap
 // import Container from 'react-bootstrap/Container';
 // import Col from 'react-bootstrap/Col';
@@ -8,6 +9,7 @@ const Landing = () => {
     return (
         <div>
             <Header></Header>
+            <Footer></Footer>
         </div>
     );
 };
