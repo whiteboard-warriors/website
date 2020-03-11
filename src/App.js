ce3f8b3834bb6abc5b2ada2bde161e631c31366f
Jordan Baucke <jordan.baucke@gmail.com>
2020-03-10 21:18:22 -0700

    initial attempt at adding static content

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, Component } from 'react'
 // router
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 // bootstrap
 // import Container from 'react-bootstrap/Container';
 // import Row from 'react-bootstrap/Row';
@@ -14,28 +15,28 @@ import Jobs from './components/pages/Jobs'
 import Resources from './components/pages/Resources'
 // components
 import NavBar from './components/layout/Navbar'
+
 // css
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.scss'
 // images
-// import logo from './img/logo/ww_logo./svg';
-// import wireframe from './img/wireframes/wireframe_1.png';
-
-function App() {
-	return (
-		<Fragment>
-			<Router>
-				<NavBar></NavBar>
-				<Switch>
-					<Route exact path="/" component={Landing} />
-					<Route exact path="/about" component={About} />
-					<Route exact path="/meetups" component={Meetups} />
-					<Route exact path="/jobs" component={Jobs} />
-					<Route exact path="/resources" component={Resources} />
-				</Switch>
-			</Router>
-		</Fragment>
-	)
+class App extends Component {
+	render() {
+		return (
+			<Fragment>
+				<Router>
+					<NavBar></NavBar>
+					<Switch>
+						<Route exact path="/" component={Landing} />
+						<Route exact path="/about" component={About} />
+						<Route exact path="/meetups" component={Meetups} />
+						<Route exact path="/jobs" component={Jobs} />
+						<Route exact path="/resources" component={Resources} />
+					</Switch>
+				</Router>
+			</Fragment>
+		)
+	}
 }
 
 export default App
