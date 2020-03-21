db4fbdbd859d407c2372095354f949d2919687e2
Jordan Baucke <jordan.baucke@gmail.com>
2020-03-21 15:40:05 -0600

    add article content and filler text to "About Page"

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,29 +1,29 @@
-import React, { Fragment } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Fragment } from 'react'
+import { Link } from 'react-router-dom'
 // bootstrap
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 // import Form from 'react-bootstrap/Form'
 // import FormControl from 'react-bootstrap/FormControl'
 // import Button from 'reaAct-bootstrap/Button'
-import './navbar.scss';
+import './navbar.scss'
 
 const NavBar = () => {
 	return (
 		<Fragment>
-			<Navbar bg='light' variant='light' expand='md'>
-				<Navbar.Toggle aria-controls='basic-navbar-nav' />
-				<Navbar.Collapse id='basic-navbar-nav'>
+			<Navbar expand="md">
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
 					<Container>
-						<Nav className='mr-auto'>
-							<Link className='nav-link' to='/'>
+						<Nav className="mr-auto">
+							<Link className="nav-link" to="/">
 								Home
 							</Link>
-							<Link className='nav-link' to='/about'>
+							<Link className="nav-link" to="/about">
 								About
 							</Link>
-							<Link className='nav-link' to='/meetups'>
+							<Link className="nav-link" to="/meetups">
 								Meetups
 							</Link>
 							{/* <Link className='nav-link' to='/jobs'>
@@ -45,7 +45,7 @@ const NavBar = () => {
 				</Navbar.Collapse>
 			</Navbar>
 		</Fragment>
-	);
-};
+	)
+}
 
-export default NavBar;
+export default NavBar
--- src/components/scenes/About.js
+++ src/components/scenes/About.js
@@ -1,13 +1,22 @@
 import React, { Fragment } from 'react'
+import Container from 'react-bootstrap/Container'
+
 import Header from '../Header'
 import Footer from '../Footer'
 import './scene.scss'
+import ArticleContent from '../ArticleContent'
+import Row from 'react-bootstrap/Row'
 
 const About = () => {
 	return (
 		<Fragment>
 			<Header></Header>
-			<div className="scene-content"></div>
+			<Container>
+				<Row className="single">
+					<ArticleContent></ArticleContent>
+				</Row>
+			</Container>
+			<Footer></Footer>
 		</Fragment>
 	)
 }
