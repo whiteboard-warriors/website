59dd8ad64b652a95a121a3cac1ffa7d875cce765
Steven Gutierrez <stematgut@gmail.com>
2020-03-23 18:29:29 -0700

    Added media query padding/margin lefts to navbar links so they wouldn't jump around at page breaks

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,36 +1,36 @@
-import React, { Fragment } from 'react'
-import { Link } from 'react-router-dom'
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 // bootstrap
-import Container from 'react-bootstrap/Container'
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import './navbar.scss'
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import './navbar.scss';
 
 const NavBar = () => {
-	return (
-		<Fragment>
-			<Navbar bg="light" variant="light" expand="md">
-				<Navbar.Toggle aria-controls="basic-navbar-nav" />
-				<Navbar.Collapse id="basic-navbar-nav">
-					<Container>
-						<Nav className="mr-auto">
-							<Link className="nav-link" to="/">
-								Home
-							</Link>
-							<Link className="nav-link" to="/about">
-								About
-							</Link>
-							<Link className="nav-link" to="/meetups">
-								Meetups
-							</Link>
-							{/* <Link className='nav-link' to='/jobs'>
+  return (
+    <Fragment>
+      <Navbar bg='light' variant='light' expand='md'>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Container className='links-container'>
+            <Nav className='mr-auto'>
+              <Link className='nav-link' to='/'>
+                Home
+              </Link>
+              <Link className='nav-link' to='/about'>
+                About
+              </Link>
+              <Link className='nav-link' to='/meetups'>
+                Meetups
+              </Link>
+              {/* <Link className='nav-link' to='/jobs'>
                                 Jobs
                             </Link>
                             <Link className='nav-link' to='/resources'>
                                 Resources
                             </Link> */}
-						</Nav>
-						{/* <Form inline>
+            </Nav>
+            {/* <Form inline>
 							<FormControl
 								type="text"
 								placeholder="Search"
@@ -38,11 +38,11 @@ const NavBar = () => {
 							/>
 							<Button variant="outline-primary">Search</Button>
 						</Form> */}
-					</Container>
-				</Navbar.Collapse>
-			</Navbar>
-		</Fragment>
-	)
-}
+          </Container>
+        </Navbar.Collapse>
+      </Navbar>
+    </Fragment>
+  );
+};
 
-export default NavBar
+export default NavBar;
