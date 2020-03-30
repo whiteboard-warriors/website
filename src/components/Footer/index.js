ac2b126ee79375e4c5a81501903419fc50cfe6d3
Steven Gutierrez <stematgut@gmail.com>
2020-03-30 16:34:44 -0700

    Added scss color name for backgorund gray and changed footer background and added margin on top for main body separation.  Added a social link area

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,65 +8,64 @@ import Col from 'react-bootstrap/Col';
 import './footer.scss';
 
 const Footer = () => {
-	const date = new Date();
-	const year = date.getFullYear();
+    const date = new Date();
+    const year = date.getFullYear();
 
-	return (
-		<Fragment>
-			<footer className='footer'>
-				<Container className='footer-top'>
-					<Row>
-						<Col xs={12} md={6}>
-							<h5 className='text-uppercase'>Footer Content</h5>
-							<p>
-								Here you can use rows and columns to organize
-								your footer content.
-							</p>
-						</Col>
-						<Col xs={6} md={3}>
-							<h5 className='text-uppercase'>Links</h5>
+    return (
+        <Fragment>
+            <footer className='footer'>
+                <Container className='footer-top'>
+                    <Row>
+                        <Col xs={12} md={6} className='footer-social'>
+                            <h5 className='text-uppercase'>Follow us:</h5>
+                            <ul>
+                                <li>FB</li>
+                                <li>TW</li>
+                                <li>INST</li>
+                            </ul>
+                        </Col>
+                        <Col xs={6} md={3}>
+                            <h5 className='text-uppercase'>Links</h5>
 
-							<ul className='list-unstyled'>
-								<li>
-									<a href='#!'>Link 1</a>
-								</li>
-								<li>
-									<a href='#!'>Link 2</a>
-								</li>
-								<li>
-									<a href='#!'>Link 3</a>
-								</li>
-								<li>
-									<a href='#!'>Link 4</a>
-								</li>
-							</ul>
-						</Col>
-						<Col xs={6} md={3}>
-							<h5 className='text-uppercase'>Links</h5>
+                            <ul className='list-unstyled'>
+                                <li>
+                                    <a href='#!'>Link 1</a>
+                                </li>
+                                <li>
+                                    <a href='#!'>Link 2</a>
+                                </li>
+                                <li>
+                                    <a href='#!'>Link 3</a>
+                                </li>
+                                <li>
+                                    <a href='#!'>Link 4</a>
+                                </li>
+                            </ul>
+                        </Col>
+                        <Col xs={6} md={3}>
+                            <h5 className='text-uppercase'>Links</h5>
 
-							<ul className='list-unstyled'>
-								<li>
-									<a href='#!'>Link 1</a>
-								</li>
-								<li>
-									<a href='#!'>Link 2</a>
-								</li>
-								<li>
-									<a href='#!'>Link 3</a>
-								</li>
-								<li>
-									<a href='#!'>Link 4</a>
-								</li>
-							</ul>
-						</Col>
-					</Row>
-				</Container>
-				<section className='footer-bottom text-center'>
-					&copy; {year} Whiteboard Warriors.
-				</section>
-			</footer>
-		</Fragment>
-	);
+                            <ul className='list-unstyled'>
+                                <li>
+                                    <a href='#!'>Link 1</a>
+                                </li>
+                                <li>
+                                    <a href='#!'>Link 2</a>
+                                </li>
+                                <li>
+                                    <a href='#!'>Link 3</a>
+                                </li>
+                                <li>
+                                    <a href='#!'>Link 4</a>
+                                </li>
+                            </ul>
+                        </Col>
+                    </Row>
+                </Container>
+                <section className='footer-bottom text-center'>&copy; {year} Whiteboard Warriors.</section>
+            </footer>
+        </Fragment>
+    );
 };
 
 export default Footer;
