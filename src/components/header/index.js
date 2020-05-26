4cc78e4e7adcf0665e775b2f2fd8da23017fde79
Pablo Motta <pmotta.dev@gmail.com>
2020-05-26 15:13:47 -0700

    new design and styles

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -6,6 +6,7 @@ import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Image from 'react-bootstrap/Image';
+import Button from 'react-bootstrap/Button';
 import './style.scss';
 
 // image
@@ -14,27 +15,29 @@ import logo from './ww_logo.svg';
 const Header = () => {
 	return (
 		<Fragment>
-			<header>
-				<Jumbotron fluid>
-					<Container>
-						<Row>
-							<Col
-								sm={{ span: 6, offset: 3 }}
-								xs={{ span: 6, offset: 3 }}
-								md={{ span: 4, offset: 4 }}
-								lg={{ span: 4, offset: 4 }}
-								xl={{ span: 4, offset: 4 }}
-							>
-								<div>
-									<Link to='/'>
-										<Image className='masthead-logo' src={logo} />
-									</Link>
-								</div>
-							</Col>
-						</Row>
-					</Container>
-				</Jumbotron>
-			</header>
+			<Jumbotron fluid>
+				<Container className='text-center'>
+					<Row>
+						<Col
+							sm={{ span: 6, offset: 3 }}
+							xs={{ span: 6, offset: 3 }}
+							md={{ span: 4, offset: 4 }}
+							lg={{ span: 4, offset: 4 }}
+							xl={{ span: 4, offset: 4 }}
+						>
+							<div>
+								<Link to='/'>
+									<Image className='masthead-logo' src={logo} />
+								</Link>
+							</div>
+						</Col>
+					</Row>
+					<h2>Software engineers of all levels coming together to sharpen their coding interview skills</h2>
+					<Button size='lg' className='btn-header-cta'>
+						Join The Meetup
+					</Button>
+				</Container>
+			</Jumbotron>
 		</Fragment>
 	);
 };
