ddcdb71b01df52616386464193d3a666fa3c688d
Jordan Baucke <jordan.baucke@gmail.com>
2021-03-07 11:33:30 -0800

    add login authenticate jwt

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import React, { Fragment } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import AuthState from './context/auth/AuthState'
+import AlertState from './context/alert/AlertState'
 
 // pages
 import Landing from './components/scenes/Landing/index'
@@ -22,34 +23,46 @@ import withTracker from './tracker/withTracker'
 // css
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.scss'
-import LoginModal from './components/LoginModal'
 
 // images
 export default function App() {
-
 	return (
 		<Fragment>
-			<AuthState>
-				<Router>
-					<NavBar></NavBar>
-					<Switch>
-						<Route exact path="/" component={withTracker(Landing)} />
-						<Route exact path="/about" component={withTracker(About)} />
-						<Route exact path="/donate" component={withTracker(Donate)} />
-						<Route exact path="/meetups" component={withTracker(Meetups)} />
-						<Route exact path="/jobs" component={withTracker(Jobs)} />
-						<Route exact path="/resources" component={withTracker(Resources)} />
-						<Route exact path="/volunteer" component={withTracker(Volunteer)} />
-						<Route
-							exact
-							path="/testimonials"
-							component={withTracker(Testimonials)}
-						/>
-						<Route exact path="/nonprofit" component={withTracker(NonProfit)} />
-						<Route exact path="/501c3" component={withTracker(NonProfit)} />
-					</Switch>
-				</Router>
-			</AuthState>
+			<AlertState>
+				<AuthState>
+					<Router>
+						<NavBar></NavBar>
+						<Switch>
+							<Route exact path="/" component={withTracker(Landing)} />
+							<Route exact path="/about" component={withTracker(About)} />
+							<Route exact path="/donate" component={withTracker(Donate)} />
+							<Route exact path="/meetups" component={withTracker(Meetups)} />
+							<Route exact path="/jobs" component={withTracker(Jobs)} />
+							<Route
+								exact
+								path="/resources"
+								component={withTracker(Resources)}
+							/>
+							<Route
+								exact
+								path="/volunteer"
+								component={withTracker(Volunteer)}
+							/>
+							<Route
+								exact
+								path="/testimonials"
+								component={withTracker(Testimonials)}
+							/>
+							<Route
+								exact
+								path="/nonprofit"
+								component={withTracker(NonProfit)}
+							/>
+							<Route exact path="/501c3" component={withTracker(NonProfit)} />
+						</Switch>
+					</Router>
+				</AuthState>
+			</AlertState>
 		</Fragment>
 	)
 }
--- client/src/components/LoginModal/index.js
+++ client/src/components/LoginModal/index.js
@@ -1,57 +1,116 @@
-import React, { Fragment, useContext, useState, useEffect } from 'react'
+import React, { Fragment, useContext, useState, useEffect, useRef } from 'react'
 import AuthContext from '../../context/auth/authContext'
-import { Modal, Button } from 'react-bootstrap'
-
+import AlertContext from '../../context/alert/alertContext'
+import { Modal, Button, Tabs, Tab, Col, Row, Form } from 'react-bootstrap'
+import Alert from '../Alerts'
 import './style.scss'
 
 export default function LoginModal(props) {
 	const authContext = useContext(AuthContext)
 	const { login, error, clearErrors, isAuthenticated } = authContext
+	const alertContext = useContext(AlertContext)
+	const { setAlert } = alertContext
+	const [key, setKey] = useState('log-in')
 
 	useEffect(() => {
-		// if (error === 'Invalid Credentials') {
-		// 	setAlert(
-		// 		"Hmmm, that didn't work please re-enter your username and password or click 'Forgot Password'",
-		// 		'danger'
-		// 	)
-		// 	clearErrors()
-		// }
-		// eslint-disable-next-line
-	}, [error, isAuthenticated, props.history])
+		if (error === 'Invalid Credentials') {
+			setAlert(
+				"Hmmm, that didn't work please re-enter your username and password or click 'Forgot Password'",
+				'danger'
+			)
+			clearErrors()
+		}
+
+	}, [])
+
+	const loginForm = useRef()
 
 	const [user, setUser] = useState({
 		email: '',
 		password: '',
 	})
-
 	const { email, password } = user
-	const handleClose = () => {}
+
+	/**
+	 *
+	 * @param {*} e
+	 */
 	const onChange = (e) => {
 		setUser({ ...user, [e.target.name]: e.target.value })
 	}
 
-	const onSubmit = (e) => {
-		e.preventDefault()
-		login({
-			email,
-			password,
-		})
+	const handleActionClick = (e) => {
+    e.preventDefault();
+		if (key === 'log-in') {
+			console.log('login')
+			login({ email, password })
+		} else {
+		}
 	}
 
 	return (
 		<Fragment>
 			<Modal {...props}>
 				<Modal.Header closeButton>
-					<Modal.Title>Modal title</Modal.Title>
+					<Modal.Title>Login to Whiteboard Warriors</Modal.Title>
 				</Modal.Header>
 
 				<Modal.Body>
-					<p>Modal body text goes here.</p>
+					<Alert />
+					<Tabs id="controlled-tab" activeKey={key} onSelect={(k) => setKey(k)}>
+						<Tab className="p-4" eventKey="log-in" title="Log In">
+							<Row>
+								<Col>
+									<Form className="form-custom-margin">
+										<Row>
+											<Col>
+												<Form.Group controlId="formEmail">
+													<Form.Control
+														type="text"
+														placeholder="E-Mail"
+														name="email"
+														value={email}
+														onChange={onChange}
+														required
+													/>
+												</Form.Group>
+											</Col>
+										</Row>
+										<Row>
+											<Col>
+												<Form.Group controlId="formPassword">
+													<Form.Control
+														type="password"
+														placeholder="Password"
+														name="password"
+														value={password}
+														onChange={onChange}
+														required
+													/>
+												</Form.Group>
+												<a href="/forgot-password">Forgot Password?</a>
+											</Col>
+										</Row>
+										{/* <div className="text-center">
+							<Button variant="primary" type="submit" size="lg">
+								Sign In
+							</Button>
+						</div> */}
+									</Form>
+								</Col>
+							</Row>
+						</Tab>
+						<Tab className="p-4" eventKey="register" title="Register"></Tab>
+					</Tabs>
 				</Modal.Body>
 
 				<Modal.Footer>
-					<Button variant="secondary">Close</Button>
-					<Button variant="primary">Save changes</Button>
+					<Button variant="secondary" onClick={props.onHide}>
+						Close
+					</Button>
+					<Button variant="primary" onClick={handleActionClick}>
+						{key === 'log-in' ? 'Login' : 'Register'}
+					</Button>
 				</Modal.Footer>
 			</Modal>
 		</Fragment>
