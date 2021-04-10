6a58b2e2f2fb0670b9622f9c97bdcb8fc860a0a2
Pablo Motta <pmotta.dev@gmail.com>
2021-04-09 23:00:21 -0700

    removed unecessary routes

---
+++ server/index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const app = express();
+
+const PORT = process.env.PORT || 5005;
+
+// Define middleware here
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(
+	session({
+		secret: 'whiteboardwarriors',
+		resave: true,
+		saveUninitialized: true,
+		// cookie: { secure: true }
+	})
+);
+app.use(passport.initialize());
+require('./config/passport')(passport);
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+	app.get('/', (req, res) => {
+		res.sendFile(process.cwd() + '/client/build/index.html');
+	});
+
+	app.get('/sitemap.xml', (req, res) => {
+		res.sendFile(process.cwd() + '/client/public/sitemap.xml');
+	});
+}
+// Add routes, both API and view
+app.use(
+	'/api/users',
+	passport.authenticate('jwt', { session: false }),
+	require('./routes/users')
+);
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/jobs', require('./routes/jobs'));
+// Connect to the Mongo DB
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/war-time', {
+	useNewUrlParser: true,
+	useFindAndModify: false,
+});
+
+// Start the API server
+app.listen(PORT, function () {
+	console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
