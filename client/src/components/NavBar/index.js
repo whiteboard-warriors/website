9987cfd5f5028601bf1edcfe2551818f9032aa3b
Pablo Motta <pmotta.dev@gmail.com>
2021-04-15 22:33:20 -0700

    added jobs link and restructured links

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,49 +1,49 @@
-import React, { Fragment, useState, useContext, useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import React, { Fragment, useState, useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 // bootstrap
-import Container from 'react-bootstrap/Container'
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import NavDropdown from 'react-bootstrap/NavDropdown'
-import AuthContext from '../../context/auth/authContext'
-import AlertContext from '../../context/alert/alertContext'
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import AuthContext from '../../context/auth/authContext';
+import AlertContext from '../../context/alert/alertContext';
 
-import './navbar.scss'
-import LoginModal from '../LoginModal'
+import './navbar.scss';
+import LoginModal from '../LoginModal';
 
 export default function NavBar(props) {
-	const [show, setShow] = useState(false)
-	const authContext = useContext(AuthContext)
-	const alertContext = useContext(AlertContext)
-	const { setAlert } = alertContext
+	const [show, setShow] = useState(false);
+	const authContext = useContext(AuthContext);
+	const alertContext = useContext(AlertContext);
+	const { setAlert } = alertContext;
 	const {
 		isAuthenticated,
 		logout,
 		user,
 		registrationSuccess,
 		clearErrors,
-	} = authContext
+	} = authContext;
 
 	useEffect(() => {
 		if (registrationSuccess) {
-			setAlert('Great Success! Welcome!', 'success')
-			setShow(false)
-			clearErrors()
+			setAlert('Great Success! Welcome!', 'success');
+			setShow(false);
+			clearErrors();
 		}
 		// eslint-disable-next-line
-	}, [registrationSuccess])
+	}, [registrationSuccess]);
 
 	const displayModal = (e) => {
-		e.preventDefault()
-		setShow(true)
-	}
+		e.preventDefault();
+		setShow(true);
+	};
 
 	const aboutTitle = (
 		<Fragment>
-			<i className="navbar-icon fas fa-info-circle"></i>
+			<i className='navbar-icon fas fa-info-circle'></i>
 			About
 		</Fragment>
-	)
+	);
 
 	const authLinks = (
 		<Fragment>
@@ -51,93 +51,104 @@ export default function NavBar(props) {
 				<Fragment>
 					<NavDropdown
 						title={user.firstName ? user.firstName : ''}
-						id="collasible-nav-dropdown"
+						id='collasible-nav-dropdown'
 					>
 						<Link
-							data-rb-event-key="/profile"
-							className="dropdown-item"
-							to="/profile"
+							data-rb-event-key='/profile'
+							className='dropdown-item'
+							to='/profile'
 						>
 							Profile
 						</Link>
 
-						<Link onClick={logout} className="dropdown-item" to="/">
+						<Link onClick={logout} className='dropdown-item' to='/'>
 							Log Out
 						</Link>
 					</NavDropdown>
-					<ul className="navbar-nav">
-						<li className="nav-item avatar">
-							<Link className="nav-link p-0" to="/profile">
+					<ul className='navbar-nav'>
+						<li className='nav-item avatar'>
+							<Link className='nav-link p-0' to='/profile'>
 								<img
-									src="https://mdbootstrap.com/img/Photos/Avatars/avatar-5.jpg"
-									className="profile-pic rounded-circle z-depth-0"
-									alt="avatar"
+									src='https://mdbootstrap.com/img/Photos/Avatars/avatar-5.jpg'
+									className='profile-pic rounded-circle z-depth-0'
+									alt='avatar'
 								></img>
 							</Link>
 						</li>
 					</ul>
 				</Fragment>
 			) : (
-				<Link onClick={displayModal} className="nav-link" to="/">
-					<i className="navbar-icon fas fa-sign-in-alt"></i>
+				<Link onClick={displayModal} className='nav-link' to='/'>
+					<i className='navbar-icon fas fa-sign-in-alt'></i>
 					Login
 				</Link>
 			)}
 		</Fragment>
-	)
+	);
 
 	return (
 		<Fragment>
 			<Navbar
-				bg="light"
-				expand="lg"
-				className="d-flex justify-content-lg-around"
+				bg='light'
+				expand='lg'
+				className='d-flex justify-content-lg-around'
 			>
-				<Navbar.Toggle aria-controls="basic-navbar-nav" />
-				<Navbar.Collapse id="basic-navbar-nav">
+				<Navbar.Toggle aria-controls='basic-navbar-nav' />
+				<Navbar.Collapse id='basic-navbar-nav'>
 					<Container>
 						<Nav>
-							<Link className="nav-link" to="/">
-								<i className="navbar-icon fas fa-user-ninja"></i>
-								Home
+							<Link className='nav-link' to='/donate'>
+								<i className='navbar-icon fas fa-donate'></i>
+								Donate
 							</Link>
-							<NavDropdown title={aboutTitle} id="basic-nav-dropdown">
-								<NavDropdown.Item as="div">
-									<Link className="nav-link" to="/about">
-										<i className="navbar-icon fas fa-info"></i>
-										Our Story
-									</Link>
-								</NavDropdown.Item>
-								<NavDropdown.Divider />
-								<NavDropdown.Item as="div">
-									<Link className="nav-link" to="/testimonials">
-										<i className="navbar-icon fas fa-bullhorn"></i>
-										Testimonials
-									</Link>
-								</NavDropdown.Item>
-								<NavDropdown.Divider />
-								<NavDropdown.Item as="div">
-									<Link className="nav-link" to="/nonprofit">
-										<i className="navbar-icon fas fa-users"></i>
-										Non-Profit
-									</Link>
-								</NavDropdown.Item>
-							</NavDropdown>
-							<Link className="nav-link" to="/meetups">
-								<i className="navbar-icon fab fa-meetup"></i>
+							<Link className='nav-link' to='/volunteer'>
+								<i className='navbar-icon fas fa-hand-paper'></i>
+								Volunteer
+							</Link>
+							<Link className='nav-link' to='/meetups'>
+								<i className='navbar-icon fab fa-meetup'></i>
 								Meetups
 							</Link>
 						</Nav>
 					</Container>
-					<Container className="d-flex justify-content-lg-end">
+					<Container className='d-flex justify-content-lg-end'>
 						<Nav>
-							<Link className="nav-link" to="/donate">
-								<i className="navbar-icon fas fa-donate"></i>
-								Donate
+							<Link className='nav-link' to='/jobs'>
+								<i className='navbar-icon fa fa-briefcase'></i>
+								Jobs
 							</Link>
-							<Link className="nav-link" to="/volunteer">
-								<i className="navbar-icon fas fa-hand-paper"></i>
-								Volunteer
+							<NavDropdown
+								title={aboutTitle}
+								id='basic-nav-dropdown'
+							>
+								<NavDropdown.Item as='div'>
+									<Link className='nav-link' to='/about'>
+										<i className='navbar-icon fas fa-info'></i>
+										Our Story
+									</Link>
+								</NavDropdown.Item>
+								<NavDropdown.Divider />
+								<NavDropdown.Item as='div'>
+									<Link
+										className='nav-link'
+										to='/testimonials'
+									>
+										<i className='navbar-icon fas fa-bullhorn'></i>
+										Testimonials
+									</Link>
+								</NavDropdown.Item>
+								<NavDropdown.Divider />
+								<NavDropdown.Item as='div'>
+									<Link className='nav-link' to='/nonprofit'>
+										<i className='navbar-icon fas fa-users'></i>
+										Non-Profit
+									</Link>
+								</NavDropdown.Item>
+							</NavDropdown>
+
+							<Link className='nav-link' to='/'>
+								<i className='navbar-icon fas fa-user-ninja'></i>
+								Home
 							</Link>
 						</Nav>
 						{authLinks}
@@ -147,5 +158,5 @@ export default function NavBar(props) {
 
 			<LoginModal show={show} onHide={() => setShow(false)}></LoginModal>
 		</Fragment>
-	)
+	);
 }
