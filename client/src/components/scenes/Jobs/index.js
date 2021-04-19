bf7d7949fba82180a315a9fd88f2b23bbaa20ce4
Pablo Motta <pmotta.dev@gmail.com>
2021-04-18 19:55:29 -0700

    jobs page

--- client/src/components/scenes/Jobs/index.js
+++ client/src/components/scenes/Jobs/index.js
@@ -1,17 +1,55 @@
 import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 import './style.scss';
 
-import Footer from '../../Footer';
+// Components
+import JobCard from './JobCard';
+// Bootstrap
+import { Container, Row, Col } from 'react-bootstrap';
+// Temp data
+import jobs from './tempJobs';
+
 const Jobs = () => {
 	return (
 		<Fragment>
-			<div className='p-5 m-5 text-center'>
-				<h1 className='p-5 m-5'>Jobs</h1>
-			</div>
-			<div className='p-5 m-5 text-center'>
-				<h1 className='p-5 m-5'>Jobs</h1>
-			</div>
-			<Footer />
+			<Container>
+				<div className='text-center'>
+					<h2 className='mt-5 mb-3'>Jobs</h2>
+				</div>
+				<Row>
+					<Col
+						lg={{ span: 2, offset: 8 }}
+						className='d-flex flex-row-reverse'
+					>
+						<Link
+							to='/jobs/post'
+							className='btn btn-primary btn-lg '
+						>
+							<b>Post a job</b>
+						</Link>
+					</Col>
+				</Row>
+				<Row className='mt-3'>
+					<Col lg={{ span: 8, offset: 2 }}>
+						{jobs.map((job) => {
+							return (
+								<JobCard
+									key={job._id}
+									jobID={job._id}
+									company={job.company}
+									title={job.title}
+									city={job.city}
+									state={job.state}
+									salary={job.salary}
+									about={job.about}
+									postDate={job.postDate}
+									admin={false}
+								/>
+							);
+						})}
+					</Col>
+				</Row>
+			</Container>
 		</Fragment>
 	);
 };
