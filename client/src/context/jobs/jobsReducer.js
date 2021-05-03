c81d7e42c1e28cb90926ae5a9858f1d22abae00a
Pablo Motta <pmotta.dev@gmail.com>
2021-05-02 19:17:22 -0700

    update state management

--- client/src/context/jobs/jobsReducer.js
+++ client/src/context/jobs/jobsReducer.js
@@ -53,11 +53,16 @@ export default (state, action) => {
 				loading: true,
 			};
 		}
-		case CREATE_JOB:
-		case UPDATE_JOB: {
+		case CREATE_JOB: {
 			return {
 				...state,
 				saving: true,
+			};
+		}
+		case UPDATE_JOB: {
+			return {
+				...state,
+				updating: true,
 			};
 		}
 		case CREATE_JOB_SUCCESS:
@@ -70,8 +75,8 @@ export default (state, action) => {
 			return {
 				...state,
 				jobs: state.jobs.map((job) => (job._id === action.payload._id ? action.payload : job)),
-				saving: false,
-				saveSuccess: true,
+				updating: false,
+				updateSuccess: true,
 			};
 
 		case CLEAR_JOBS:
@@ -89,6 +94,8 @@ export default (state, action) => {
 				saveSuccess: false,
 				loading: false,
 				saving: false,
+				updating: false,
+				updateSuccess: false,
 				deleting: false,
 				deleteSuccess: false,
 			};
