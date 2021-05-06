8bc58480ba889a45a675e94e04019d276606a498
Jordan Baucke <jordan.baucke@gmail.com>
2021-05-05 18:25:02 -0700

    fix release scripts

--- scripts/deploy_staging.sh
+++ scripts/deploy_staging.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 openssl aes-256-cbc -K $encrypted_13c03eac0538_key -iv $encrypted_13c03eac0538_iv -in .env-staging.enc -out .env-staging -d && mv .env-staging .env && rsync -r --delete-after --quiet -e "ssh -o StrictHostKeyChecking=no" $TRAVIS_BUILD_DIR/ $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH_STAGING
