35957853c352853ec6edeedf19a3be57c86c7c02
Jordan Baucke <jordan.baucke@gmail.com>
2021-05-05 18:56:45 -0700

    update script

--- scripts/deploy_production.sh
+++ scripts/deploy_production.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-
-openssl aes-256-cbc -K $encrypted_13c03eac0538_key -iv $encrypted_13c03eac0538_iv -in .env-production.enc -out .env-staging -d && mv .env-staging .env && rsync -r --delete-after --quiet -e "ssh -o StrictHostKeyChecking=no" $TRAVIS_BUILD_DIR $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
+echo "Stopping Website..."
+ssh -o StrictHostKeyChecking=no $DEPLOY_USER@$DEPLOY_HOST "forever stop website"
+echo "Deploy Staging, decrypting encrypted .env file"
+openssl aes-256-cbc -K $encrypted_13c03eac0538_key -iv $encrypted_13c03eac0538_iv -in .env-production.enc -out .env-staging -d
+echo "Placing env file..."
+mv .env-prod .env 
+echo "Copying to server..."
+rsync -r --delete-after --quiet -e "ssh -o StrictHostKeyChecking=no" $TRAVIS_BUILD_DIR $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
+echo "Restarting website..."
+ssh -o StrictHostKeyChecking=no $DEPLOY_USER@$DEPLOY_HOST "cd htdocs/website && forever start -a --uid 'website' server/index.js"
--- scripts/deploy_staging.sh
+++ scripts/deploy_staging.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-echo "Deploy Staging..."
-openssl aes-256-cbc -K $encrypted_13c03eac0538_key -iv $encrypted_13c03eac0538_iv -in .env-staging.enc -out .env-staging -d && mv .env-staging .env && rsync -r --delete-after --quiet -e "ssh -o StrictHostKeyChecking=no" $TRAVIS_BUILD_DIR/ $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH_STAGING
+echo "Stopping staging-website"
+ssh -o StrictHostKeyChecking=no $DEPLOY_USER@$DEPLOY_HOST "forever stop website-staging"
+openssl aes-256-cbc -K $encrypted_13c03eac0538_key -iv $encrypted_13c03eac0538_iv -in .env-staging.enc -out .env-staging -d 
+echo "Placing env file..."
+mv .env-staging .env
+echo "Copying to server..."
+rsync -r --delete-after --quiet -e "ssh -o StrictHostKeyChecking=no" $TRAVIS_BUILD_DIR/ $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH_STAGING
+echo "Restarting staging-website"
+ssh -o StrictHostKeyChecking=no $DEPLOY_USER@$DEPLOY_HOST "cd htdocs/website-staging && forever start -a --uid 'website-staging' server/index.js"
