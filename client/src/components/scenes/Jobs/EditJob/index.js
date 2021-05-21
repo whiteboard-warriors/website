b31797e70037e29052127e5ef3f3e5fc95fe4abe
Pablo Motta <pmotta.dev@gmail.com>
2021-05-20 20:06:50 -0700

    replaced props.history with useHistory

--- client/src/components/scenes/Jobs/EditJob/index.js
+++ client/src/components/scenes/Jobs/EditJob/index.js
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import AlertContext from '../../../../context/alert/alertContext';
 import AuthContext from '../../../../context/auth/authContext';
 import JobsContext from '../../../../context/jobs/jobsContext';
@@ -9,19 +10,20 @@ import JobForm from '../JobForm';
 import './style.scss';
 
 const EditJob = (props) => {
+	const history = useHistory();
 	const alertContext = useContext(AlertContext);
 	const authContext = useContext(AuthContext);
 	const jobsContext = useContext(JobsContext);
 
 	const { setAlert } = alertContext;
-	const { job, updateJob, error, clearJobError, updateSuccess, clearCreateJobFlags, getJob, loading } = jobsContext;
+	const { job, updateJob, error, clearJobError, updateSuccess, clearJobFlags, getJob, loading } = jobsContext;
 	const { user, isAuthenticated } = authContext;
 
 	useEffect(() => {
 		getJob(props.match.params.jobID);
 
 		if (!isAuthenticated) {
-			props.history.push('/');
+			history.push('/');
 			setAlert("Oops, looks like you're not logged in 😱. Please login or sign up to perform this action.", 'danger');
 		}
 
@@ -31,15 +33,15 @@ const EditJob = (props) => {
 		}
 		if (updateSuccess) {
 			setAlert('Your job has been updated.', 'success');
-			clearCreateJobFlags();
-			props.history.push(`/jobs/user/${user._id}`);
+			clearJobFlags();
+			history.push(`/jobs/user/${user._id}`);
 		}
 		if (user.jobPosting === 'no') {
 			setAlert('Please update your profile to be able to post jobs :)', 'warning');
-			props.history.push('/profile');
+			history.push('/profile');
 		}
 		//eslint-disable-next-line
-	}, [error, isAuthenticated, props.history, updateSuccess]);
+	}, [error, isAuthenticated, history, updateSuccess]);
 
 	return <>{loading || !job ? <Spinner /> : <JobForm action={updateJob} job={job} />}</>;
 };
