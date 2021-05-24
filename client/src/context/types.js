3f38bc7bf0c6acb9d78080d09464aa7ff4c705bd
Pablo Motta <pmotta.dev@gmail.com>
2021-05-23 19:13:45 -0700

    clear login flags type

--- client/src/context/types.js
+++ client/src/context/types.js
@@ -34,6 +34,7 @@ export const SIGNUP_FAIL = 'SIGNUP_FAIL';
 export const USER_LOADED = 'USER_LOADED';
 export const AUTH_ERROR = 'AUTH_ERROR';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const CLEAR_LOGIN_FLAGS = 'CLEAR_LOGIN_FLAGS';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
 export const LOGOUT = 'LOGOUT';
 export const CLEAR_ERRORS = 'CLEAR_ERRORS';
