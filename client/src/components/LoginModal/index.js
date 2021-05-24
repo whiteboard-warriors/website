1cba9bcbb75bbcd3523b538b07b78db2314b87e6
Pablo Motta <pmotta.dev@gmail.com>
2021-05-23 17:00:44 -0700

    close modal when clicking outside of it

--- client/src/components/LoginModal/index.js
+++ client/src/components/LoginModal/index.js
@@ -11,38 +11,40 @@ export default function LoginModal(props) {
 
 	return (
 		<Fragment>
-			<Modal show={show}>
-				<Modal.Header closeButton>
-					<Modal.Title>Login to Whiteboard Warriors</Modal.Title>
-				</Modal.Header>
+			<div onClick={() => setShow(false)}>
+				<Modal show={show}>
+					<Modal.Header closeButton>
+						<Modal.Title>Login to Whiteboard Warriors</Modal.Title>
+					</Modal.Header>
 
-				<Modal.Body>
-					<Alert />
+					<Modal.Body>
+						<Alert />
 
-					<Tabs id='controlled-tab' activeKey={key} onSelect={(k) => setKey(k)}>
-						<Tab className='p-4' eventKey='log-in' title='Log In'>
-							<LoginForm></LoginForm>
-						</Tab>
-						<Tab className='p-4' eventKey='register' title='Register'>
-							<RegistrationForm></RegistrationForm>
-						</Tab>
-					</Tabs>
-				</Modal.Body>
+						<Tabs id='controlled-tab' activeKey={key} onSelect={(k) => setKey(k)}>
+							<Tab className='p-4' eventKey='log-in' title='Log In'>
+								<LoginForm></LoginForm>
+							</Tab>
+							<Tab className='p-4' eventKey='register' title='Register'>
+								<RegistrationForm></RegistrationForm>
+							</Tab>
+						</Tabs>
+					</Modal.Body>
 
-				<Modal.Footer>
-					<Button variant='secondary' onClick={() => setShow(false)}>
-						Close
-					</Button>
-					<Button
-						onClick={() => setShow(false)}
-						variant='primary'
-						type='submit'
-						form={key === 'log-in' ? 'loginForm' : 'registrationForm'}
-					>
-						{key === 'log-in' ? 'Login' : 'Register'}
-					</Button>
-				</Modal.Footer>
-			</Modal>
+					<Modal.Footer>
+						<Button variant='secondary' onClick={() => setShow(false)}>
+							Close
+						</Button>
+						<Button
+							onClick={() => setShow(false)}
+							variant='primary'
+							type='submit'
+							form={key === 'log-in' ? 'loginForm' : 'registrationForm'}
+						>
+							{key === 'log-in' ? 'Login' : 'Register'}
+						</Button>
+					</Modal.Footer>
+				</Modal>
+			</div>
 		</Fragment>
 	);
 }
