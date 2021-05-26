e73c5bf1ca03a15522dce79ad347b99a3fac9899
Pablo Motta <pmotta.dev@gmail.com>
2021-05-26 16:59:55 -0700

    new 404 page

--- client/src/components/scenes/PageNotFound/index.js
+++ client/src/components/scenes/PageNotFound/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import wwLogo from '../../../img/logo/ww_logo.svg';
+
+import './style.scss';
 
 const PageNotFound = () => {
-	return (
-		<div>
-			<h1>404 - Page Not Found</h1>
-		</div>
-	);
+    return (
+        <>
+            <Container>
+                <Row>
+                    <Col lg={{ span: 6, offset: 3 }}>
+                        <div className='PNF-card'>
+                            <h1> 404</h1>
+                            <h2 className='text-center'>oops.. not even the ninjas could find this page</h2>
+                            <img src={wwLogo} alt='whiteboard warrior ninjas' />
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    );
 };
 
 export default PageNotFound;
