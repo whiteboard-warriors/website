a9188557c69b7c1a0ca675107ebe507b63eec9db
Pablo Motta <pmotta.dev@gmail.com>
2021-06-02 21:30:52 -0700

    not needed

--- client/src/stories/2-Map.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import MeetupMap from '../components/meetup-map/MeetupMap'
-
-export default {
-	title: 'MeetupMap',
-	component: MeetupMap
-}
-
-export const Map = () => <MeetupMap />
