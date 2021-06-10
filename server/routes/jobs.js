0012f18593bd65d1a2e060afab7498c100a0ec13
Pablo Motta <pmotta.dev@gmail.com>
2021-06-09 22:07:09 -0700

    new job fields to job routes

--- server/routes/jobs.js
+++ server/routes/jobs.js
@@ -44,9 +44,22 @@ router.get('/created-by/user/', async (req, res) => {
 // @route   POST /api/jobs
 // @desc     creates a job
 router.post('/', async (req, res) => {
-	let { company, title, city, state, salary, about } = req.body;
-	console.log(req.body);
-	console.log(req.user);
+	let {
+		company,
+		title,
+		city,
+		state,
+		salary,
+		about,
+		remote,
+		visaSponsorship,
+		hardRequirement1,
+		hardRequirement2,
+		hardRequirement3,
+		softRequirement1,
+		softRequirement2,
+		softRequirement3,
+	} = req.body;
 
 	try {
 		// check to make sure user making updates has job posting rights.
@@ -65,6 +78,14 @@ router.post('/', async (req, res) => {
 			state,
 			salary,
 			about,
+			remote,
+			visaSponsorship,
+			hardRequirement1,
+			hardRequirement2,
+			hardRequirement3,
+			softRequirement1,
+			softRequirement2,
+			softRequirement3,
 			postDate: new Date(),
 		});
 		await job.save();
@@ -78,7 +99,24 @@ router.post('/', async (req, res) => {
 // @route   PUT /api/jobs/admin
 // @desc    Allows Admin to update job
 router.put('/:id', async (req, res) => {
-	let { company, title, city, state, salary, about, active, postDate } = req.body;
+	let {
+		company,
+		title,
+		city,
+		state,
+		salary,
+		about,
+		active,
+		remote,
+		visaSponsorship,
+		hardRequirement1,
+		hardRequirement2,
+		hardRequirement3,
+		softRequirement1,
+		softRequirement2,
+		softRequirement3,
+		postDate,
+	} = req.body;
 	try {
 		// check to make sure user making updates has job posting rights.
 		let user = await db.User.findOne({ _id: req.user.id });
@@ -95,6 +133,14 @@ router.put('/:id', async (req, res) => {
 		if (salary) job.salary = salary;
 		if (about) job.about = about;
 		if (active) job.active = active;
+		if (remote) job.remote = remote;
+		if (visaSponsorship) job.visaSponsorship = visaSponsorship;
+		if (hardRequirement1) job.hardRequirement1 = hardRequirement1;
+		if (hardRequirement2) job.hardRequirement2 = hardRequirement2;
+		if (hardRequirement3) job.hardRequirement3 = hardRequirement3;
+		if (softRequirement1) job.softRequirement1 = softRequirement1;
+		if (softRequirement2) job.softRequirement2 = softRequirement2;
+		if (softRequirement3) job.softRequirement3 = softRequirement3;
 		if (postDate) job.postDate = new Date();
 
 		await db.Job.findOneAndUpdate({ _id: req.params.id }, { $set: job });
@@ -127,8 +173,8 @@ router.post('/apply-for-job', async (req, res) => {
 				msg: 'Please update your user profile and include your LinkedIn link to apply for jobs',
 			});
 		}
-		// sendJobApplicationEmail(applicantName, applicantEmail, linkedIn, employerName, employerEmail)
-		sendJobApplicationEmail(user.firstName, user.email, user.linkedIn, job.createdBy.firstName, job.createdBy.email);
+		// sendJobApplicationEmail = (applicantName, applicantEmail, linkedIn, githubUsername, employerName, employerEmail)
+		sendJobApplicationEmail(user.firstName, user.email, user.linkedIn, user.githubUsername, job.createdBy.firstName, job.createdBy.email);
 
 		res.send('Your job application was submitted successfully');
 	} catch (err) {
