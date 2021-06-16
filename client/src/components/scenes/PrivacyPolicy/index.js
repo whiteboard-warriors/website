061f1435db2f726f59ee4c773bfa3ef41a0ecae6
Pablo Motta <pmotta.dev@gmail.com>
2021-06-15 22:13:25 -0700

    lorem ipsum content - to be replaced.

--- client/src/components/scenes/PrivacyPolicy/index.js
+++ client/src/components/scenes/PrivacyPolicy/index.js
@@ -15,21 +15,31 @@ const index = () => {
 				</div>
 				<Row>
 					<Col md={{ span: 8, offset: 2 }} className='mt4r about'>
-						<h4>Who has access to my personal data?</h4>
+						<h4>Excepteur sint occaecat non proident?</h4>
 						<p className='lead'>
-							Even though freeCodeCamp has thousands of volunteers, none of those people have access to your private data.
-							freeCodeCamp has a few full-time staff, some of whom work directly on our databases. They have the ability to view
-							your private data, but only do so when providing you with technical support. As for the personal data that you choose
-							to share on your developer portfolio, anyone on the internet can see it by navigating to your developer portfolio's
-							public URL. Again, we've given you full control over what parts of your developer profile are public.
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+							aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+							Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
 						</p>
-						<h4>Who has access to my personal data?</h4>
+						<h4>Lorem ipsum dolor sit amet?</h4>
 						<p className='lead'>
-							Even though freeCodeCamp has thousands of volunteers, none of those people have access to your private data.
-							freeCodeCamp has a few full-time staff, some of whom work directly on our databases. They have the ability to view
-							your private data, but only do so when providing you with technical support. As for the personal data that you choose
-							to share on your developer portfolio, anyone on the internet can see it by navigating to your developer portfolio's
-							public URL. Again, we've given you full control over what parts of your developer profile are public.
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+							aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+							Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+							occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+						</p>
+						<h4>Excepteur sint occaecat non proident?</h4>
+						<p className='lead'>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+							aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+							Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+						</p>
+						<h4>Lorem ipsum dolor sit amet?</h4>
+						<p className='lead'>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+							aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+							Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+							occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 						</p>
 					</Col>
 				</Row>
