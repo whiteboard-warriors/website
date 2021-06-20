be4bf0fc571aa8c8dcb4fe8c291d5ea85409543c
Jordan Baucke <jordan.baucke@gmail.com>
2021-06-19 19:21:39 -0700

    updates to sitemap and add "Secret"

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
 	session({
-		secret: 'whiteboardwarriors',
+		secret: process.env.SECRET,
 		resave: true,
 		saveUninitialized: true,
 		// cookie: { secure: true }
