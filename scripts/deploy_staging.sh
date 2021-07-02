d5df09cdb70722b32960745b796c5d3980d24148
Jordan Baucke <jordan.baucke@gmail.com>
2021-07-02 16:26:12 -0700

    modify deploy path

--- scripts/deploy_staging.sh
+++ scripts/deploy_staging.sh
@@ -4,5 +4,5 @@ openssl aes-256-cbc -K $encrypted_13c03eac0538_key -iv $encrypted_13c03eac0538_i
 echo "Placing env file..."
 mv .env-staging .env
 echo "Copying to server..."
-rsync -r --delete-after --quiet -e "ssh -o StrictHostKeyChecking=no" $TRAVIS_BUILD_DIR/ $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH_STAGING
+rsync -r --delete-after --quiet -e "ssh -o StrictHostKeyChecking=no" $TRAVIS_BUILD_DIR $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH_STAGING
 echo "Done running copy..."
