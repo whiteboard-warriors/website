6373294d34f93f12f960b41ec845bddb696818b1
Jordan Baucke <jordan.baucke@gmail.com>
2021-07-02 17:21:34 -0700

    update

--- scripts/deploy_staging.sh
+++ scripts/deploy_staging.sh
@@ -3,6 +3,8 @@ echo "Decrypting .env file"
 openssl aes-256-cbc -K $encrypted_13c03eac0538_key -iv $encrypted_13c03eac0538_iv -in .env-staging.enc -out .env-staging -d 
 echo "Placing env file..."
 mv .env-staging .env
+echo "Running Build..."
+npm run-script build
 echo "Copying to server..."
-rsync -r --delete-after -e "ssh -o StrictHostKeyChecking=no" $TRAVIS_BUILD_DIR $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH_STAGING
+rsync -a --delete-after -e "ssh -o StrictHostKeyChecking=no" $TRAVIS_BUILD_DIR $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH_STAGING
 echo "Done running copy..."
