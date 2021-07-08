82109474ecea3cff9bc1fc0a1d8e3455f46bed84
Jordan Baucke <jordan.baucke@gmail.com>
2021-07-08 16:22:10 -0700

    deploy path

--- scripts/deploy_staging.sh
+++ scripts/deploy_staging.sh
@@ -3,11 +3,10 @@ echo "Decrypting .env file"
 openssl aes-256-cbc -K $encrypted_13c03eac0538_key -iv $encrypted_13c03eac0538_iv -in .env-staging.enc -out .env-staging -d 
 echo "Placing env file..."
 mv .env-staging .env
-echo "Running Build..."
-npm run-script build
 echo "Client directory contents: "
 cd client && ls
 echo $TRAVIS_BUILD_DIR 
+echo "Deploy path staging: " $DEPLOY_PATH_STAGING
 cd $TRAVIS_BUILD_DIR/client && ls
 echo $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH_STAGING
 echo "Copying to server..."
